c9c31a2400821f90f83b1bf32bf9c8eec2ec1ac2
Ghostscape <m.smith@opmbx.org>
2015-06-15 23:55:51 +0100

    end day 1

---
+++ src/main/java/com/ghost/initial/initial.java
@@ -0,0 +1,17 @@
+package com.ghost.initial;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+/**
+ * Created by Phantom on 15/06/2015.
+ */
+@Mod(
+        modid = "initial",
+        name = "initial",
+        version = "1.8-1.0"
+)
+public class initial {
+}
